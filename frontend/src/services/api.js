527b91b3097a8d899567e36382fd211630eebbad
Mizan <mizanpfi@gmail.com>
2026-03-12 19:39:45 +0600

    Fix double https in API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE = 'https://https://edunexus-api-4bd7.onrender.com/api/v1'
+const API_BASE = 'https://edunexus-api-4bd7.onrender.com/api/v1'
 
 const api = axios.create({
   baseURL: API_BASE,
